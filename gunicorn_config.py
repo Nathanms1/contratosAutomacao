7f1197bc823859f39e2290ed6e7eabee8618c6d5
Nathan Maria <nathanms2000@outlook.com>
2024-02-09 01:58:55 -0300

    ajuste

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,4 @@
-bind = '0.0.0.0:8080'
-workers = 4
+from app import app
+
+bind = '0.0.0.0:8000'
+workers = 2
--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,3 @@
-from os.path import abspath, dirname
-import sys
-
-# Adicione o diretório raiz do projeto ao path do Python
-sys.path.insert(0, abspath(dirname(__file__)))
-
-# Importe o aplicativo Flask da maneira correta
 from app import app as application
 
 if __name__ == "__main__":
