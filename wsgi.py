a00302cf876499846d9807efd88563acb44aaa5c
Nathan Maria <nathanms2000@outlook.com>
2024-02-09 01:51:08 -0300

    ajuste

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,23 @@
-from app import app as application
+from app import app
+from gunicorn.app.base import BaseApplication
 
-if __name__ == "__main__":
-    application.run()
+class FlaskApp(BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        for key, value in self.options.items():
+            if key in self.cfg.settings and value is not None:
+                self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+if __name__ == '__main__':
+    options = {
+        'bind': '0.0.0.0:8080',
+        'workers': 4,
+    }
+    FlaskApp(app, options).run()
